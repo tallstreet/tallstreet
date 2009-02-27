c65f3dff00b117129cfa98eff266ea9a92f47b21
Gary Roberts <gary@fishpond.co.nz>
2009-02-27 20:53:44 +1300

    Update to appengine-patch-1.0

--- manage.py
+++ manage.py
@@ -7,24 +7,12 @@ if __name__ == '__main__':
     from mediautils.compilemessages import updatemessages
     updatemessages()
 
-    # Regenerate media files
-    import sys
-    from mediautils.generatemedia import updatemedia, generatemedia
-    if len(sys.argv) < 2 or sys.argv[1] not in ('generatemedia', 'update'):
-        updatemedia()
-
     # Generate compressed media files for manage.py update
+    import sys
+    from mediautils.generatemedia import updatemedia
     if len(sys.argv) >= 2 and sys.argv[1] == 'update':
-        generatemedia(True)
+        updatemedia(True)
 
     import settings
     from django.core.management import execute_manager
-    try:
-        execute_manager(settings)
-    finally:
-        # Regenerate uncompressed media files after manage.py update
-        import sys
-        from mediautils.generatemedia import generatemedia
-        if len(sys.argv) >= 2 and sys.argv[1] == 'update':
-            print 'Regenerating uncompressed media'
-            generatemedia(False, silent=True)
+    execute_manager(settings)
